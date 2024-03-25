ae9cb019e9562f7f1929da8449c8d333b4f07c5d
Ryan Jensen <rjensen@raschnet.com>
2024-03-25 11:27:51 -0500

    Initial commit

---
+++ test/domw2024-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Domw2024Cdk from '../lib/domw2024-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/domw2024-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Domw2024Cdk.Domw2024CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
