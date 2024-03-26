16b68420c0022fb98972dab56ef1e18214965a3c
Ryan Jensen <rjensen@raschnet.com>
2024-03-26 13:29:39 -0500

    - add network stack (commented) for more fluid presentation transition

--- bin/domw2024-cdk.ts
+++ bin/domw2024-cdk.ts
@@ -2,8 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { HostedZoneStack } from '../lib/hostedzone-stack';
+// import { NetworkStack } from '@sketchdev/cdk';
 
 const app = new cdk.App();
 new HostedZoneStack(app, 'Domw2024HostedZone', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
+
+// new NetworkStack(app, 'Domw2024NetworkVpc', {
+//   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// });
